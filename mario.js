d1e6230270338760ca56fbedb3ebf698ecbfc089
paullee100 <leepaul863@gmail.com>
2023-01-04 14:24:55 -0800

    animation test

--- mario.js
+++ mario.js
@@ -3,7 +3,9 @@ class Mario {
         Object.assign(this, {game});
 
         this.game.mario = this;
-        this.state = 0; // idle = 0, running = 1, flying = 2
+        this.state = 0; // idle = 0, walking = 1, spinning = 3
+
+        this.animation = new Animator(this.spritesheet, 248, 76, 16, 32, 2, 0.5, 14, false, true);
 
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/mario.png");
     };
@@ -13,6 +15,7 @@ class Mario {
     };
 
     draw(ctx) {
-        ctx.drawImage(this.spritesheet, 208, 76, 16, 32, 0, 0, 48, 96);
+        //ctx.drawImage(this.spritesheet, 208, 76, 16, 32, 0, 0, 48, 96);
+        this.animation.drawFrame(this.game.clockTick, ctx, 0, 0, 3);s
     };
 };
